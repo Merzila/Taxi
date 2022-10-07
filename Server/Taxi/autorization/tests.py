6415db9985fe1ce03a36ba6bd3b5fba9d5e93b9e
Merzila <andryushka.zakharov.04@mail.ru>
2022-10-07 23:20:52 +0700

    creating test

--- Server/Taxi/autorization/tests.py
+++ Server/Taxi/autorization/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+import requests
 
 # Create your tests here.
+
+response = requests.post("http://127.0.0.1:8000/registration", data = {
+                                                                        "user": "users",
+                                                                        "phone": "81234567890",
+                                                                        "name": "admin",
+                                                                        "password": "admin"
+                                                                    })
+print(response.text)
+
+response = requests.post("http://127.0.0.1:8000/autorization", data = {
+                                                                        "user": "users",
+                                                                        "phone": "81234567890",
+                                                                        "password": "admin"
+                                                                    })
+print(response.text)
