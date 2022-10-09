402edfde7a4b2a3081c2fc03b793d7718768c971
Merzila <andryushka.zakharov.04@mail.ru>
2022-10-09 18:02:26 +0700

    Test

--- Server/Taxi/route/tests.py
+++ Server/Taxi/route/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
-
+import requests
 # Create your tests here.
+
+response = requests.get("http://127.0.0.1:8000/56.452513,84.974931,53.712258,87.801965")
+print(response.text)
