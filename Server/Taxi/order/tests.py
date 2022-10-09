1fa06c52f14412f08b2e98a9334fbcd6177a860c
Merzila <andryushka.zakharov.04@mail.ru>
2022-10-09 22:25:53 +0700

    Test of order

--- Server/Taxi/order/tests.py
+++ Server/Taxi/order/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
-
+import requests
 # Create your tests here.
+
+response = requests.post('http://127.0.0.1:8000/order', data = {
+                                                                "id_user": 1,
+                                                                "address_start": "улица Фёдора Лыткина Томск",
+                                                                "address_end": "улица Горького 12 Мыски",
+                                                                "ordered_time": "22:00",
+                                                                "tariff": "comfort",
+                                                                "payment": "MirPay",
+                                                                "wishes": "Маленький ребёнок"
+                                                                })
+print(response.text)
